b28ff300a1d925b22385366f2ae40f8364cd162a
Willsem <happymainer@gmail.com>
2020-11-10 15:08:30 +0300

    :pencil: complete the solve

--- sem_02/lab_03/src/main.py
+++ sem_02/lab_03/src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, session, request
+import numpy as np
 
 app = Flask(__name__)
 app.secret_key = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
@@ -10,24 +11,24 @@ def index(count=1):
     if "username" not in session:
         session["username"] = "user"
         session["data"] = []
-        session["result"] = None
+        session["result"] = []
     session["count"] = count
-    return render_template("index.html", session=session, solve=False)
+    return render_template("index.html", session=session)
 
 
 @app.route("/plus")
 def add_count():
     session["count"] += 1
-    session["result"] = None
-    return render_template("index.html", session=session, solve=False)
+    session["result"] = []
+    return render_template("index.html", session=session)
 
 
 @app.route("/minus")
 def remove_count():
     if session["count"] > 1:
         session["count"] -= 1
-        session["result"] = None
-    return render_template("index.html", session=session, solve=False)
+        session["result"] = []
+    return render_template("index.html", session=session)
 
 
 @app.route("/", methods=["POST"])
@@ -43,7 +44,25 @@ def solve():
             s = request.form[str(i * count + j)]
             data[i].append(int(s) if s != "" else 0)
     session["data"] = data
-    return render_template("index.html", session=session, solve=True)
+
+    coef = np.zeros((count, count))
+    summ = np.zeros(count)
+
+    for i in range(count):
+        summ[i] = sum(data[i])
+        for j in range(count):
+            coef[i][j] = data[j][i]
+        coef[i][i] = -summ[i]
+
+    m = np.zeros(count)
+    m[-1] = 1
+    coef[-1] = 1
+    out = np.linalg.solve(coef, m)
+    session["relative_result"] = out.tolist()
+    time = (1 - out) / out / summ
+    session["result"] = time.tolist()
+
+    return render_template("index.html", session=session)
 
 
 if __name__ == "__main__":
